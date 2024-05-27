8d5f0ece252f168b683b3d122376de0ba7bdbc2f
andMp <myposk@gmail.com>
2024-05-27 20:43:41 +0300

    stvor zminih

--- Source.cpp
+++ Source.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 int main()
 {
+	float a, b;
+	cout << "Vvedit znach a: ";
+	cin >> a;
+	cout << "Vvedit znach b: ";
+	cin >> b;
+	cout << "Oberit bazanu diyu: \n";
+	char var;
+	do {
 
+	} while (var != 27);
 }
