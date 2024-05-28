1d711d42e08d6796ce4b949dd31e298b34aaf070
andMp <myposk@gmail.com>
2024-05-28 11:30:54 +0300

    programa gotova

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<conio.h>
 using namespace std;
 int main()
 {
@@ -10,6 +11,35 @@ int main()
 	cout << "Oberit bazanu diyu: \n";
 	char var;
 	do {
-
+		system("cls");
+		cout << "\tOberit bazanu diyu:\n";
+		cout << " + \n";
+		cout << " - \n";
+		cout << " * \n";
+		cout << " / \n";
+		cout << "Esc - Vihid.\n";
+		var = _getch();
+		switch (var)
+		{
+		case '+':system("cls");
+			cout << a << " + " << b << " = " << a + b<<'\n';
+			system("pause");
+			break;
+		case '-':system("cls");
+			cout << a << " - " << b << " = " << a - b << '\n';
+			system("pause");
+			break;
+		case '*':system("cls");
+			cout << a << " * " << b << " = " << a * b << '\n';
+			system("pause");
+			break;
+		case '/':system("cls");
+			if (b != 0)
+				cout << a << " / " << b << " = " << a / b << '\n';
+			else
+				cout << "Na \"0\" ne dilat!\n";
+			system("pause");
+			break;
+		}
 	} while (var != 27);
 }
